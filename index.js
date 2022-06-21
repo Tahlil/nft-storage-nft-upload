1406c9b956d6e795ab8c363bdcb6f2fe123d09c0
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-22 00:16:03 +0600

    Retrieve nft names and descriptions

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ const fs = require('fs');
 const nftStorageToken = process.env.NFT_STORAGE_TOKEN;
 const client = new NFTStorage({ token: nftStorageToken })
 
-const directoryPath = path.join(__dirname, 'images');
+const imageDirectoryPath = path.join(__dirname, 'images');
+const nameDirectoryPath = path.join(__dirname, 'info', 'name.txt');
+const descriptionDirectoryPath = path.join(__dirname, 'info', 'description.txt');
 
-fs.readdir(directoryPath, function (err, files) {
+const nftNames = fs.readFileSync(nameDirectoryPath, 'utf-8').split("\r\n");
+const nftDescriptions = fs.readFileSync(descriptionDirectoryPath, 'utf-8').split("\r\n");
+
+
+fs.readdir(imageDirectoryPath, function (err, files) {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
     console.log("File names:");
     files.forEach(function (file) {
-        console.log(file); 
+        // console.log(file); 
     });
 });
