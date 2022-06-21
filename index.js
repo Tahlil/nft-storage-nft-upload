c41611f8984596b452b998e13b69152c02a27080
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-22 00:02:46 +0600

    Read images file names

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 require("dotenv").config();
-const { NFTStorage } = require('nft.storage')
 const { NFTStorage, File, Blob } = require('nft.storage')
+
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
 
 const nftStorageToken = process.env.NFT_STORAGE_TOKEN;
 const client = new NFTStorage({ token: nftStorageToken })
+
+const directoryPath = path.join(__dirname, 'images');
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    console.log("File names:");
+    files.forEach(function (file) {
+        console.log(file); 
+    });
+});
