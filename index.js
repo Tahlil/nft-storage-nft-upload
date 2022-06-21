5daf77e50a723e4430f286f07525d8f7986b9bcd
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-21 23:56:26 +0600

    Add imports

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 require("dotenv").config();
+const { NFTStorage } = require('nft.storage')
+const { NFTStorage, File, Blob } = require('nft.storage')
+
 const nftStorageToken = process.env.NFT_STORAGE_TOKEN;
 console.log(nftStorageToken);
